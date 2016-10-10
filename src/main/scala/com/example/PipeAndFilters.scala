bc5e5d9810097587b7ec520e68a9b0d57be78bd0
shigemk2 <i.am.shige@gmail.com>
2016-10-10 17:21:40 +0900

    Add classes extending actor class

--- src/main/scala/com/example/PipeAndFilters.scala
+++ src/main/scala/com/example/PipeAndFilters.scala
@@ -3,7 +3,72 @@ package com.example
 import akka.actor._
 import com.example._
 
-object PipeAndFiltersDriver {
-  def main(args: Array[String]): Unit = {
+case class ProcessIncomingOrder(orderInfo: Array[Byte])
+
+object PipeAndFiltersDriver extends CompletableApp(9) {
+}
+
+class Authenticator(nextFilter: ActorRef) extends Actor {
+  def receive = {
+    case message: ProcessIncomingOrder =>
+      val text = new String(message.orderInfo)
+      println(s"Authenticator: processing $text")
+      val orderText = text.replace("(certificate)", "")
+      nextFilter ! ProcessIncomingOrder(orderText.toCharArray.map(_.toByte))
+      PipeAndFiltersDriver.completedStep()
+  }
+}
+
+class Decrypter(nextFilter: ActorRef) extends Actor {
+  def receive = {
+    case message: ProcessIncomingOrder =>
+      val text = new String(message.orderInfo)
+      println(s"Decrypter: processing $text")
+      val orderText = text.replace("(encryption)", "")
+      nextFilter ! ProcessIncomingOrder(orderText.toCharArray.map(_.toByte))
+      PipeAndFiltersDriver.completedStep()
+  }
+}
+
+class Deduplicator(nextFilter: ActorRef) extends Actor {
+  val processedOrderIds = scala.collection.mutable.Set[String]()
+
+  def orderIdFrom(orderText: String): String = {
+    val orderIdIndex = orderText.indexOf("id='") + 4
+    val orderIdLastIndex = orderText.indexOf("'", orderIdIndex)
+    orderText.substring(orderIdIndex, orderIdLastIndex)
+  }
+
+  def receive = {
+    case message: ProcessIncomingOrder =>
+      val text = new String(message.orderInfo)
+      println(s"Deduplicator: processing $text")
+      val orderId = orderIdFrom(text)
+      if (processedOrderIds.add(orderId)) {
+        nextFilter ! message
+      } else {
+        println(s"Deduplicator: found duplicate order $orderId")
+      }
+      PipeAndFiltersDriver.completedStep()
+  }
+}
+
+class OrderAcceptanceEndpoint(nextFilter: ActorRef) extends Actor {
+  def receive = {
+    case message: ProcessIncomingOrder =>
+      val text = new String(message.orderInfo)
+      println(s"OrderAcceptanceEndpoint: processing $text")
+      val orderText = text.replace("(encryption)", "")
+      nextFilter ! ProcessIncomingOrder(orderText.toCharArray.map(_.toByte))
+      PipeAndFiltersDriver.completedStep()
+  }
+}
+
+class OrderManagementSystem extends Actor {
+  def receive = {
+    case message: ProcessIncomingOrder =>
+      val text = new String(message.orderInfo)
+      println(s"OrderManagementSystem: processing unique order: $text")
+      PipeAndFiltersDriver.completedStep()
   }
 }
