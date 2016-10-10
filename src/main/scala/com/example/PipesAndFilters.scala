4ce46414745399b657fb397abd04f215bf93bbe2
shigemk2 <i.am.shige@gmail.com>
2016-10-10 17:33:39 +0900

    Add PipesAndFiltersDriver

--- src/main/scala/com/example/PipesAndFilters.scala
+++ src/main/scala/com/example/PipesAndFilters.scala
@@ -6,6 +6,21 @@ import com.example._
 case class ProcessIncomingOrder(orderInfo: Array[Byte])
 
 object PipesAndFiltersDriver extends CompletableApp(9) {
+  val orderText = "(encryption)(certificate)<order id '123'>...</order>"
+  val rawOrderBytes = orderText.toCharArray.map(_.toByte)
+
+  val filter5 = system.actorOf(Props[OrderManagementSystem], "orderManagementSystem")
+  val filter4 = system.actorOf(Props(classOf[Deduplicator], filter5), "deduplicator")
+  val filter3 = system.actorOf(Props(classOf[Authenticator], filter4), "authenticator")
+  val filter2 = system.actorOf(Props(classOf[Decrypter], filter3), "decrypter")
+  val filter1 = system.actorOf(Props(classOf[OrderAcceptanceEndpoint], filter2), "orderAcceptanceEndpoint")
+
+  filter1 ! rawOrderBytes
+  filter1 ! rawOrderBytes
+
+  awaitCompletion
+
+  println("PipesAndFilters: is completed.")
 }
 
 class Authenticator(nextFilter: ActorRef) extends Actor {
